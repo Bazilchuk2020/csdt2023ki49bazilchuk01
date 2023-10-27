2f3aca53d19c763d6d104e6ba823c27e388a4a1d
Bazilchuk2020 <146776000+Bazilchuk2020@users.noreply.github.com>
2023-10-27 21:06:38 +0300

    Finished task2

---
+++ ConsoleApp18/ConsoleApp18/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO.Ports;
+
+SerialPort serialPort = new SerialPort("COM3", 9600);
+serialPort.Open();
+var message1 = Console.ReadLine();
+var message2 = Console.ReadLine();
+
+await Task.Delay(1000);
+
+
+serialPort.DataReceived += (sender, e) =>
+{
+    if (sender is SerialPort sender1)
+    {
+        Console.WriteLine($"sender: {sender1.ReadExisting()}");
+    }
+};
+
+serialPort.Write(message1);
+serialPort.Write(message2);
+
+await Task.Delay(25000);
